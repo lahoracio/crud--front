30c348c54a1449603b69a583b73b3582974c8756
LARA MACHADO HORÁCIO <24122307@CFP127ALUNO.LOCAL>
2025-05-15 15:49:26 -0300

    tudo feito

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 
 import { getContatos, getContatosPorNome, postContato } from "./contatos.js"
 
+import { uploadImageToAzure } from "./uploadImageToAzure.js"
+
 function criarCard(contato){
     const container = document.getElementById('container')
     const card = document.createElement('div')
@@ -45,10 +47,18 @@ function voltarHome(){
 }
 
 async function salvarContato(){
+
+    const uploadParams = {
+        file: document.getElementById('foto').files[0],
+        storageAccount: 'uploadtutorial',
+        sasToken: 'sp=racwl&st=2025-05-15T17:30:24Z&se=2025-05-16T01:30:24Z&sv=2024-11-04&sr=c&sig=pJkhIPyVg93b%2BAF8%2FNSCLs5W5hV%2BM3GBsOFwOUV8zsE%3D',
+        containerName: 'fotos',
+    };
+    
     const contato = {
         "nome": document.getElementById('nome').value,
         "celular": document.getElementById('celular').value,
-        "foto": document.getElementById('foto').value,
+        "foto": await uploadImageToAzure(uploadParams),
         "email": document.getElementById('email').value,
         "endereco": document.getElementById('endereco').value,
         "cidade": document.getElementById('cidade').value
@@ -59,7 +69,7 @@ async function salvarContato(){
         await exibirContatos()
         voltarHome()
         alert ('Cadastro realizado com sucesso!!!')
-
+        
     }
 }
 exibirContatos()
